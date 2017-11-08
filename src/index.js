aef03a61b7025578a57412e81f6ea22747dc9923
shrekuu <shrekuu@gmail.com>
2017-11-08 16:35:05 +0800

    add icon file-loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.jpg';
 
 function component() {
   var element = document.createElement('div');
@@ -7,6 +8,11 @@ function component() {
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
