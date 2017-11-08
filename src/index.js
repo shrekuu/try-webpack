fff021ca0d4b06324dbe05e5cd72fa363853620f
shrekuu <shrekuu@gmail.com>
2017-11-08 17:57:09 +0800

    added HashedModuleIdsPlugin

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from './data.xml';
+import Print from './print.js';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack', ''], ' ');
-  element.classList.add('hello');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  var myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = 'Click me and check the console!';
+  element.onclick = Print.bind(null, 'Hello webpack!');
 
-  element.appendChild(myIcon);
-
-  var elHeart = document.createElement('i');
-  elHeart.classList.add('fa-heart');
-  elHeart.classList.add('fa');
-  element.appendChild(elHeart);
-
-  console.log(Data);
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,49 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+// var ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+
+    // 第三方依赖列表
+    vendor: [
+      'lodash'
+    ]
+  },
+  plugins: [
+
+    // 清空 dist 目录
+    new CleanWebpackPlugin(['dist']),
+
+    // 编译 html
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    }),
+
+
+    // new ManifestPlugin(),
+
+    // 缓存变化不大的第三方依赖
+    // new webpack.NamedModulesPlugin(),
+    new webpack.HashedModuleIdsPlugin(),
+
+    // 把第三方依赖放一起
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'runtime'
+    })
+  ],
+
   output: {
-    filename: 'bundle.js',
+
+    // 输出文件名带 hash 的文件
+    filename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: [
-          'csv-loader'
-        ]
-      },
-      {
-        test: /\.xml$/,
-        use: [
-          'xml-loader'
-        ]
-      }
-    ]
-  }
 };
