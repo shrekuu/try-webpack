ea68af625afeff8c421a4713c4bf4d2195e27be4
shrekuu <shrekuu@gmail.com>
2017-11-09 11:53:06 +0800

    add Hot Module Replacement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import Print from './print.js';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
@@ -8,11 +8,22 @@ function component() {
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   btn.innerHTML = 'Click me and check the console!';
-  element.onclick = Print.bind(null, 'Hello webpack!');
+  element.onclick = printMe.bind(null, 'Hello webpack!');
 
   element.appendChild(btn);
 
   return element;
 }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
+let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
+document.body.appendChild(element);
+
+// if (module.hot) {
+module.hot.accept('./print.js', function () {
+  console.log('Accepting the updated printMe module!');
+  printMe();
+  document.body.removeChild(element);
+  element = component(); // 重新渲染页面后，component 更新 click 事件处理
+  document.body.appendChild(element);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,9 @@ module.exports = {
     app: './src/index.js',
 
     // 第三方依赖列表
-    vendor: [
-      'lodash'
-    ]
+    // vendor: [
+    //   'lodash',
+    // ]
   },
 
   // source map, 需要时再开启
@@ -20,7 +20,8 @@ module.exports = {
 
   // webpack dev server live reloading
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true,
   },
 
   plugins: [
@@ -30,36 +31,40 @@ module.exports = {
 
     // 编译 html
     new HtmlWebpackPlugin({
-      // title: 'Output Management'
+      title: 'Hot Module Replacement'
     }),
 
 
     // new ManifestPlugin(),
 
     // 缓存变化不大的第三方依赖
-    // new webpack.NamedModulesPlugin(),
-    new webpack.HashedModuleIdsPlugin(),
+    new webpack.NamedModulesPlugin(),
+    // new webpack.HashedModuleIdsPlugin(),
+
+    new webpack.HotModuleReplacementPlugin(),
 
     // 把第三方依赖放一起
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
-    }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'vendor'
+    // }),
 
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'runtime'
-    }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'runtime'
+    // }),
 
     // 配置哪些文件不需要 source map
-    new webpack.SourceMapDevToolPlugin({
-      filename: '[name].js.map',
-      exclude: [/^vendor\..+\.js$/]
-    }),
+    // new webpack.SourceMapDevToolPlugin({
+    //   filename: '[name].js.map',
+    //   exclude: [/^vendor\..+\.js$/]
+    // }),
   ],
 
   output: {
 
     // 输出文件名带 hash 的文件
-    filename: '[name].[chunkhash].js',
+    // filename: '[name].[chunkhash].js',
+    // filename: '[name].[hash].js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
 };
